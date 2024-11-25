49b5c0c52c677c4e28aacf62efe256d437e9ea04
meryem21-sys <73745781+meryem21-sys@users.noreply.github.com>
2024-11-25 11:55:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-document.querySelectorAll('.project-card').forEach(card => {
-    card.addEventListener('click', function() {
-        const projectId = this.getAttribute('data-project');
-        alert(`Détails du projet ${projectId}: Ce projet est encore en développement!`);
-    });
-});
+function toggleDetails(id) {
+    const details = document.getElementById(id);
+    details.classList.toggle('active');
+}
